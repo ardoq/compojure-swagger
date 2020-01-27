ca0d25eaee64409a50027e77848727c216d5e12f
Sigurd Totland <sigurdtotland@gmail.com>
2020-01-27 15:55:17 +0100

    Refactor remove-spec-namespaces into transform-spec-names

--- src/ardoq_swagger/util.clj
+++ src/ardoq_swagger/util.clj
@@ -1,19 +1,24 @@
 (ns ardoq-swagger.util
   (:require [clojure.walk :as walk]))
 
+(defn transform-spec-names
+  "Traverse a swagger spec removing leading namespaces in spec names"
+  [swagger-spec transformer]
+  (walk/postwalk (fn [form] (-> form
+                                (#(if-let [name (:name %)]
+                                    (assoc % :name (transformer name)) %))
+                                (#(if-let [required (get-in % [:schema :required])]
+                                    (assoc-in % [:schema :required] (->> required (map transformer) vec)) %))
+                                (#(if-let [properties (get-in % [:schema :properties])]
+                                    (assoc-in % [:schema :properties]
+                                              (reduce-kv
+                                                (fn [m k v]
+                                                  (assoc m (transformer k) v)) {} properties)) %))))
+                 swagger-spec))
+
 (defn strip-namespace [s] (if (string? s) (name (symbol s)) s))
 
 (defn remove-spec-namespaces
   "Traverse a swagger spec removing leading namespaces in spec names"
   [swagger-spec]
-  (walk/postwalk (fn [form] (-> form
-                                (#(if-let [name (:name %)]
-                                    (assoc % :name (strip-namespace name)) %))
-                                (#(if-let [required (get-in % [:schema :required])]
-                                    (assoc-in % [:schema :required] (->> required (map strip-namespace) vec)) %))
-                                (#(if-let [properties (get-in % [:schema :properties])]
-                                    (assoc-in % [:schema :properties]
-                                              (reduce-kv
-                                                (fn [m k v]
-                                                  (assoc m (strip-namespace k) v)) {} properties)) %))))
-                 swagger-spec))
+  (transform-spec-names swagger-spec strip-namespace))
