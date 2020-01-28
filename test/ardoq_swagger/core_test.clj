10afb63f1a33a41ad218617ba1fe0ed420f9ca66
Sigurd Totland <sigurdtotland@gmail.com>
2020-01-28 12:03:48 +0100

    Move spec-tools-test into ardoq-swagger.spec-test ns

--- test/ardoq_swagger/core_test.clj
+++ test/ardoq_swagger/core_test.clj
@@ -173,25 +173,3 @@
                                              :description nil,
                                              :parameters  [],
                                              :responses   {200 {:schema nil, :description ""}}}}}})))))
-
-(deftest spec-tools-test
-  (testing "spec-tools/merge works"
-    (s/def ::first-name string?)
-    (s/def ::last-name string?)
-    (s/def ::id int?)
-    (s/def ::person-spec (s/keys :req [::first-name ::last-name]))
-    (s/def ::identifiable (s/keys :req [::id]))
-    (let [handler (swagger/with-swagger
-                    (swagger/POST "/test1/:id" [])
-                    {:summary  "it works"
-                     :response {:spec (st/merge ::person-spec ::identifiable)}})]
-      (is (= (swagify-route handler)
-             {"/test1/:id" {:post {:summary "it works",
-                                   :description nil,
-                                   :parameters [],
-                                   :responses {200 {:schema {:type "object",
-                                                             :properties {"first-name" {:type "string"},
-                                                                          "last-name" {:type "string"},
-                                                                          "id" {:type "integer", :format "int64"}},
-                                                             :required ["first-name" "id" "last-name"]},
-                                                    :description ""}}}}})))))
---
+++ test/ardoq_swagger/spec_test.clj
@@ -0,0 +1,27 @@
+(ns ardoq-swagger.spec-test
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as s]
+            [ardoq-swagger.core :as swagger]
+            [spec-tools.core :as st]))
+
+(deftest spec-tools-test
+  (testing "spec-tools/merge works"
+    (s/def ::first-name string?)
+    (s/def ::last-name string?)
+    (s/def ::id int?)
+    (s/def ::person-spec (s/keys :req [::first-name ::last-name]))
+    (s/def ::identifiable (s/keys :req [::id]))
+    (let [handler (swagger/with-swagger
+                    (swagger/POST "/test1/:id" [])
+                    {:summary  "it works"
+                     :response {:spec (st/merge ::person-spec ::identifiable)}})]
+      (is (= (swagger/swagify-route handler)
+             {"/test1/:id" {:post {:summary "it works",
+                                   :description nil,
+                                   :parameters [],
+                                   :responses {200 {:schema {:type "object",
+                                                             :properties {"first-name" {:type "string"},
+                                                                          "last-name" {:type "string"},
+                                                                          "id" {:type "integer", :format "int64"}},
+                                                             :required ["first-name" "id" "last-name"]},
+                                                    :description ""}}}}})))))
