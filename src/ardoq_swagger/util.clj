00954ec59a8a8c3e4e06d87d7eb865c3f431912f
Sigurd Totland <sigurdtotland@gmail.com>
2020-01-29 12:29:43 +0100

    Make remove-spec-namespaces more idiomatic

--- src/ardoq_swagger/util.clj
+++ src/ardoq_swagger/util.clj
@@ -1,27 +1,29 @@
 (ns ardoq-swagger.util
   (:require [clojure.walk :as walk]))
 
-(defn- get-contained-spec-names-transformer [transformer]
-  (fn [form] (-> form
-                 (#(if-let [name (:name %)]
-                     (assoc % :name (transformer name)) %))
-                 (#(if-let [required (get-in % [:schema :required])]
-                     (assoc-in % [:schema :required] (->> required (map transformer) vec)) %))
-                 (#(if-let [properties (get-in % [:schema :properties])]
-                     (assoc-in % [:schema :properties]
-                               (reduce-kv
-                                 (fn [m k v]
-                                   (assoc m (transformer k) v)) {} properties)) %)))))
+(defn update-some
+  "Like `update` but only if `m` contains `k`."
+  [m k f & args]
+  (if (some? (k m))
+    (apply update m k f args)
+    m))
 
-(defn transform-spec-names
-  "Traverse a swagger spec removing leading namespaces in spec names"
-  [swagger-spec transformer]
-  (let [transform-contained-spec-names (get-contained-spec-names-transformer transformer)]
-    (walk/postwalk transform-contained-spec-names swagger-spec)))
+(defn transform-schema [schema transformer]
+  (-> schema
+      (update-some :required (partial mapv transformer))
+      (update-some :properties (partial reduce-kv
+                                   (fn [m k v]
+                                     (assoc m (transformer k) v)) {}))))
+
+(defn transform-contained-spec-names [transformer form]
+  (if (map? form)
+    (-> form (update-some :name transformer) (update-some :schema transform-schema transformer))
+    form))
+
 
 (defn strip-namespace [s] (if (string? s) (name (symbol s)) s))
 
 (defn remove-spec-namespaces
   "Traverse a swagger spec removing leading namespaces in spec names"
   [swagger-spec]
-  (transform-spec-names swagger-spec strip-namespace))
+  (walk/postwalk (partial transform-contained-spec-names strip-namespace) swagger-spec))
